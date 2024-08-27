8cfaafb5ce8d545650e01185d2801d10e46b4b6e
JaceRider <jacerider@gmail.com>
2024-08-27 13:27:49 -0500

    Prep for generator.

---
+++ neo_base/install/neo/back/src/js/back.ts
@@ -0,0 +1,11 @@
+
+// (function (Drupal, once) {
+
+//   Drupal.behaviors.back = {};
+//   Drupal.behaviors.back.attach = (context: HTMLElement) => {
+//     console.log('Backend', context, once);
+//   }
+
+// })(Drupal, once);
+
+export {};
---
+++ neo_base/install/neo/front/src/js/front.ts
@@ -0,0 +1,8 @@
+// (function (Drupal, once) {
+//   Drupal.behaviors.front = {};
+//   Drupal.behaviors.front.attach = (context: HTMLElement) => {
+//     console.log('Frontend', context, once);
+//   };
+// })(Drupal, once);
+
+export {};
